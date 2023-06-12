adbd34f11bfa65d912785eb051b94913d6f88ed7
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-06-11 23:34:29 -0500

    adding Hero Section and Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
 import Image from 'next/image';
 
+import { Hero } from '@/components';
+
 export default function Home() {
-	return <main className='overflow-hidden'></main>;
+	return (
+		<main className='overflow-hidden'>
+			<Hero />
+			<div className='mt-12 padding-x padding-y max-width' id='discover'>
+				<div className='home__text-container'>
+					<h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+					<p>Explore the cars you might like</p>
+				</div>
+				<div className='home_filters'>
+					<div className='home_filter-container'></div>
+				</div>
+			</div>
+		</main>
+	);
 }
+
+// Min 50:00
